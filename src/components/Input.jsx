6d51a425ce0b1e47cebf03803e0f72f750a7569d
KATSIARYNA YURKEVICH <77401014+KaterinaYurkevich@users.noreply.github.com>
2022-08-08 09:57:35 +0300

    Delete src directory

--- src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-function Input() {
-    
-    return <input type="text" placeholder="Добавить список">
-        </input>
-}
-
-export default Input;
--- src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-function List() {
-    
-    return (<ul class="todos">
-    <li><span class="todo-text">Вымыть кухню</span></li>
-    <li><span class="todo-text">Пойти в театр</span></li>
-</ul>)
-}
-
-export default List;
