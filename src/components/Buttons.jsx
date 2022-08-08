47baaa341177b1c212741c02a1948ff80c886d3b
KATSIARYNA YURKEVICH <77401014+KaterinaYurkevich@users.noreply.github.com>
2022-08-08 09:55:39 +0300

    Add files via upload

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,10 @@
+function Buttons() {
+    
+    return (<div id="buttons">
+    <button class="save">Сохранить</button>
+    <button class="clear">Очистить</button>
+    <button class="showTips">Справка</button>
+</div>)
+}
+
+export default Buttons;
---
+++ src/components/List.jsx
@@ -0,0 +1,9 @@
+function List() {
+    
+    return (<ul class="todos">
+    <li><span class="todo-text">Вымыть кухню</span></li>
+    <li><span class="todo-text">Пойти в театр</span></li>
+</ul>)
+}
+
+export default List;
